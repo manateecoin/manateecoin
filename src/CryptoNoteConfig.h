c9ef9cf04caec56d1ca9b2d2fb98bcd93d234295
Pierre Kim <admin@manateeshome.com>
2017-11-19 18:18:00 +0900

    1.2.0.1

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -123,7 +123,8 @@ __attribute__((unused))
 const std::initializer_list<CheckpointData> CHECKPOINTS = {
   { 24552, "a5fced9c59f3f3192fc5460845aa5ef89de9e9704a98fb5c692e23e9315d5048" },
   { 86863, "9f49f2cfe5b232b208b2b019070bb4c4b6efe4a95979be6188a2272b6a264cc9" },
-  { 87229, "83acd3eba879a378fc9763b6459fa43cbb10abc08b65d3a5b2b686b7144a3180" }
+  { 87229, "83acd3eba879a378fc9763b6459fa43cbb10abc08b65d3a5b2b686b7144a3180" },
+  { 90846, "24097098a82e7c5ce24fae16fc86cb649dc7065d5d974f53bb2eba80c9d64091" }
 };
 }
 
